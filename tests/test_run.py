e5dd72fb7988775c5582d378edd6fa098449a6b4
Franklin <brasafrank@cy-tech.fr>
2026-03-11 14:46:29 +0100

    Fix tests

--- tests/test_run.py
+++ tests/test_run.py
@@ -1,28 +1,10 @@
-"""
-This module contains example tests for a Kedro project.
-Tests should be placed in ``src/tests``, in modules that mirror your
-project's structure, and in files named test_*.py.
-"""
-
-from pathlib import Path
-
-import pytest
-from kedro.framework.session import KedroSession
-from kedro.framework.startup import bootstrap_project
-
-# The tests below are here for the demonstration purpose
-# and should be replaced with the ones testing the project
-# functionality
+from purchase_predict.pipeline_registry import register_pipelines
 
 
-class TestKedroRun:
-    def test_kedro_run_no_pipeline(self):
-        # This example test expects a pipeline run failure, since
-        # the default project template contains no pipelines.
-        bootstrap_project(Path.cwd())
+def test_register_pipelines_contains_expected_pipelines():
+    pipelines = register_pipelines()
 
-        with pytest.raises(Exception) as excinfo:
-            with KedroSession.create(project_path=Path.cwd()) as session:
-                session.run()
-
-        assert "Pipeline contains no nodes" in str(excinfo.value)
+    assert "loading" in pipelines
+    assert "processing" in pipelines
+    assert "training" in pipelines
+    assert "__default__" in pipelines
